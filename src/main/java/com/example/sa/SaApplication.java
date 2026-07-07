2cfbbdac3f0c70b6ebf7ea98ac76d46efbe6fe20
theuz <matheuslima030597ma@gmail.com>
2026-07-07 19:46:10 -0300

    teste

--- src/main/java/com/example/sa/SaApplication.java
+++ src/main/java/com/example/sa/SaApplication.java
@@ -8,7 +8,8 @@ public class SaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaApplication.class, args);
-		System.out.println("Hello Word");
+		
+		System.out.println("Hello word");
 	}
 
 }
