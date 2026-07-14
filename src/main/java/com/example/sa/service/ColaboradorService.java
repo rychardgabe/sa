05508e51d66d3b3e51a412bd77d39e3cfe5e3594
theuz <matheuslima030597ma@gmail.com>
2026-07-14 20:47:31 -0300

    Adiciona service Emprestimo crud

---
+++ src/main/java/com/example/sa/service/ColaboradorService.java
@@ -0,0 +1,5 @@
+package com.example.sa.service;
+
+public class ColaboradorService {
+
+}
---
+++ src/main/java/com/example/sa/service/EmprestimoService.java
@@ -0,0 +1,52 @@
+package com.example.sa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.sa.entity.*;
+import com.example.sa.repository.*;
+
+public class EmprestimoService {
+
+	
+	@Autowired
+	EmprestimoRepository EmprestimoRepository;
+
+	//Create
+	public void cadastrarEmprestimo(Emprestimo emprestimo) {
+		EmprestimoRepository.save(emprestimo);
+	}
+	
+	
+	
+	//Read
+	public List<Emprestimo> listaEmprestimo() {
+		return EmprestimoRepository.findAll();
+	}
+	
+	
+	public Emprestimo obterEmprestimoPorId(int id) {
+		return EmprestimoRepository.findById(id).orElseThrow(() ->new RuntimeException("Emprestimo não encontrado"));
+	}
+	
+	
+	//Update
+	public void atualizarEmprestimo(Emprestimo emprestimo) {
+		EmprestimoRepository.save(emprestimo);
+	}
+	
+	//delete
+	public void deletarEmprestimo(int id){ 
+		EmprestimoRepository.deleteById(id);
+	}
+	
+	
+	
+	
+	
+	
+}
+
+	
+
