2304c65a53e910190a625339303c7e59dffe07b0
theuz <matheuslima030597ma@gmail.com>
2026-07-14 19:57:24 -0300

    Adiciona entity Emprestimo

--- src/main/java/com/example/sa/entity/Emprestimo.java
+++ src/main/java/com/example/sa/entity/Emprestimo.java
@@ -1,5 +1,55 @@
 package com.example.sa.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "emprestimo")
 public class Emprestimo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_Emprestimo")
+	private int idEmprestimo;
+
+	@Column(name = "nome_Emprestimo")
+	private String nomeEmprestimo;
+
+	public Emprestimo(int idEmprestimo, String nomeEmprestimo) {
+		super();
+		this.idEmprestimo = idEmprestimo;
+		this.nomeEmprestimo = nomeEmprestimo;
+	}
+
+	public Emprestimo() {
+
+	}
+
+	public int getIdEmprestimo() {
+		return idEmprestimo;
+	}
+
+	public void setIdEmprestimo(int idEmprestimo) {
+		this.idEmprestimo = idEmprestimo;
+	}
+
+	public String getNomeEmprestimo() {
+		return nomeEmprestimo;
+	}
+
+	public void setNomeEmprestimo(String nomeEmprestimo) {
+		this.nomeEmprestimo = nomeEmprestimo;
+	}
+
+	@Override
+	public String toString() {
+		return "Emprestimo [idEmprestimo=" + idEmprestimo + ", nomeEmprestimo=" + nomeEmprestimo + "]";
+	}
 
 }
+
+
