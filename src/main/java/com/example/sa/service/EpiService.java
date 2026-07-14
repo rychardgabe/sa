fda98042285cdbeffe235b7970f4463fbb2d0690
theuz <matheuslima030597ma@gmail.com>
2026-07-14 20:59:42 -0300

    Adicionar service Epi crud

--- src/main/java/com/example/sa/service/EpiService.java
+++ src/main/java/com/example/sa/service/EpiService.java
@@ -1,5 +1,48 @@
 package com.example.sa.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.sa.entity.Colaborador;
+import com.example.sa.entity.Epi;
+import com.example.sa.repository.ColaboradorRepository;
+import com.example.sa.repository.*;
+
 public class EpiService {
 
+	@Autowired
+	EpiRepository EpiRepository;
+
+	//Create
+	public void cadastrarEpi(Epi epi) {
+		EpiRepository.save(epi);
+	}
+	
+	
+	
+	//Read
+	public List<Epi> listaEpi() {
+		return EpiRepository.findAll();
+	}
+	
+	
+	public Epi obterEpiPorId(int id) {
+		return EpiRepository.findById(id).orElseThrow(() ->new RuntimeException(" Epi não encontrado"));
+	}
+	
+	
+	//Update
+	public void atualizarEpi(Epi epi) {
+		EpiRepository.save(epi);
+	}
+	
+	//delete
+	public void deletarEpi(int id){ 
+		EpiRepository.deleteById(id);
+	}
+	
+
 }
+	
+
