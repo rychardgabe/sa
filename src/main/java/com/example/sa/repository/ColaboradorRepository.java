ed915268e10475fa5c314cf3197c5484c246923d
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-07-14 19:28:47 -0300

    Atualiza Repository

---
+++ src/main/java/com/example/sa/repository/ColaboradorRepository.java
@@ -0,0 +1,9 @@
+package com.example.sa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.sa.Colaborador;
+
+public interface ColaboradorRepository extends JpaRepository<Colaborador, Integer>{
+
+}
--- src/main/java/com/example/sa/repository/Coloborador.java
+++
@@ -1,9 +0,0 @@
-package com.example.sa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sa.Emprestimo;
-
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer>{
-
-}
