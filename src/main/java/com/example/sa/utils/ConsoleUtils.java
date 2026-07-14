30e4403331977d4a15ab8aa4c099a622ab21e6cd
Rychard Gabriel <rychard.futurefocus@gmail.com>
2026-07-14 19:25:51 -0300

    Adiciona testes

--- src/main/java/com/example/sa/utils/ConsoleUtils.java
+++ src/main/java/com/example/sa/utils/ConsoleUtils.java
@@ -2,4 +2,18 @@ package com.example.sa.utils;
 
 public class ConsoleUtils {
 
+	public static void exibirMenu() {
+		System.out.print("\n1) Criar colaborador\n");
+		System.out.print("2) Listar colaboradores\n");
+		System.out.print("3) Atualizar colaborador\n");
+		System.out.print("4) Deletar colaborador\n");
+		System.out.print("5) Criar epi\n");
+		System.out.print("6) Listar epis\n");
+		System.out.print("7) Atualizar epi\n");
+		System.out.print("8) Deletar epi\n");
+		System.out.print("9) Criar empréstimo\n");
+		System.out.print("10) Listar empréstimo\n");
+		System.out.print("0) Sair\n");
+	}
+
 }
